a22df543663f1e8991fff9cb5c7d8a4bf791fed0
Shivam <shivam.chahar@webkorps.com>
2026-05-07 15:55:07 +0000

    Added file organizer shell script

---
+++ organizer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p Images
+mkdir -p Documents
+mkdir -p Videos
+
+echo "Folders created!"
+#!/bin/bash
+
+mkdir -p Images
+mkdir -p Documents
+mkdir -p Videos
+
+mv *.jpg Images/
+
+echo "Done!"
